36b1a145fe620086d007666687a7ce418136c8ab
AndreiPavlov1988 <ai_pavlov@mail.ru>
2025-10-19 19:19:01 +0300

    task2

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,24 @@ public class Main {
         } else {
             System.out.println("Неизвестная операционная система");
         }
+//task2
+        int clientOS1 = 1;
+        int clientDeviceYear = 2014;
+        boolean isLiteVersion = clientDeviceYear < 2015;
+        if (clientOS1 == 0) {
+            if (isLiteVersion) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            }
+        } else if (clientOS1 == 1) {
+            if (isLiteVersion) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            }
+        } else {
+            System.out.println("Неизвестная операционная система");
+        }
     }
 }
