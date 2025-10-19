463cf3ca71b47fa4dd695f4b62b0bbd9c5cea395
AndreiPavlov1988 <ai_pavlov@mail.ru>
2025-10-19 20:23:35 +0300

    task5

--- src/Main.java
+++ src/Main.java
@@ -54,5 +54,40 @@ public class Main {
             System.out.println("Доставки нет");
         }
         System.out.println("Потребуется дней: " + deliveryDays);
+//task5
+        int monthNumber = 12;
+        String season;
+        if (monthNumber < 1 || monthNumber > 12) {
+            System.out.println("Ошибка: номер месяца должен быть от 1 до 12");
+                    return;
+                }
+
+                switch (monthNumber) {
+                    case 1:
+                    case 2:
+                    case 12:
+                        season = "зима";
+                        break;
+                    case 3:
+                    case 4:
+                    case 5:
+                        season = "весна";
+                        break;
+                    case 6:
+                    case 7:
+                    case 8:
+                        season = "лето";
+                        break;
+                    case 9:
+                    case 10:
+                    case 11:
+                        season = "осень";
+                        break;
+                    default:
+                        season = "неизвестный сезон";
+                        break;
+                }
+
+                System.out.println("Месяц №" + monthNumber + " принадлежит к сезону: " + season);
+            }
     }
-}
