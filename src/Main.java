992f4c55bc081ae63ce6f2a081a59aa03b5f9f15
AndreiPavlov1988 <ai_pavlov@mail.ru>
2025-10-20 18:21:10 +0300

    fixed task 1 and 2

--- src/Main.java
+++ src/Main.java
@@ -2,31 +2,29 @@ public class Main {
     public static void main(String[] args) {
 // task1
         int clientOS = 1;
-        if (clientOS == 0) {
-            System.out.println("Установите версию приложения для iOS по ссылке");
-        } else if (clientOS == 1) {
-            System.out.println("Установите версию приложения для Android по ссылке");
-        } else {
-            System.out.println("Неизвестная операционная система");
+        if (clientOS == 0) {System.out.println("Установите версию приложения для iOS по ссылке");
         }
+        if (clientOS == 1) {System.out.println("Установите версию приложения для Android по ссылке");
+        }
+
 //task2
         int clientOS1 = 1;
         int clientDeviceYear = 2014;
         boolean isLiteVersion = clientDeviceYear < 2015;
-        if (clientOS1 == 0) {
-            if (isLiteVersion) {
-                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-            } else {
-                System.out.println("Установите версию приложения для iOS по ссылке");
-            }
-        } else if (clientOS1 == 1) {
-            if (isLiteVersion) {
-                System.out.println("Установите облегченную версию приложения для Android по ссылке");
-            } else {
-                System.out.println("Установите версию приложения для Android по ссылке");
-            }
-        } else {
-            System.out.println("Неизвестная операционная система");
+        if (clientOS1 == 0 && isLiteVersion) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        }
+
+        if (clientOS1 == 0 && isLiteVersion) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+
+        if (clientOS1 == 1 && isLiteVersion) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
+
+        if (clientOS1 == 1 && isLiteVersion) {
+            System.out.println("Установите версию приложения для Android по ссылке");
         }
 //task3
         int year = 2021;
